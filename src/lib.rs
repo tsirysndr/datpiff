e026c1b303be079d0658a86c3316c7aae6b00464
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-08-25 10:27:14 +0300

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! # datpiff crate
+//!
+//! <p>
+//! <a href="https://crates.io/crates/datpiff" target="_blank">
+//!  <img src="https://img.shields.io/crates/v/datpiff.svg" />
+//! </a>
+//! <a href="https://docs.rs/datpiff" target="_blank">
+//!  <img src="https://docs.rs/datpiff/badge.svg" />
+//! </a>
+//! <a href="LICENSE" target="_blank">
+//!  <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-blue.svg" />
+//! </a>
+//! </p>
+//!
+//! Datpiff is a Rust library for scraping the DatPiff website for the latest mixtapes.
+//!
 pub mod constants;
 pub mod formater;
 pub mod parser;
